ad165669e42c35c5b58f57bddcbba7ce1cabafa6
Victorine Penot <vpenot001@bordeaux-inp.fr>
2023-10-24 14:08:06 +0200

    modif bp.m

--- BP.m
+++ BP.m
@@ -1,11 +1,53 @@
-function [] = BP(trame_BEC,H)
+function [msg] = BP(H, G)
 
-[lambda,pho] = calcul_degre(H);
+[lambda,pho,deg_lambda,deg_pho] = calcul_degre(H);
+
+[h,w] =  size(G);
+trame = randi([0,1],1, h);
+mot_code = (trame *G);
+p=0.7;
+eps=-1;
+msg = BEC(mot_code,p,eps)';
+for i=1:length(msg)
+
+    if msg(i) == 0
+        Lx(i) = 1e4;
+    elseif msg(i) == eps
+        Lx(i) = 0;  
+     elseif msg(i) == 1
+        Lx(i) = -1e4;
+    end
 
 end
 
+[L_xc,L_cx]=calcul_Lxc(Lx, 5, H);
+
+tp=1;
+end
+
 % Fonction calcul de L x vers c
+% 
+function[L_xc,L_cx]=calcul_Lxc(L_x, Nit,H)
+    
+    [h,w] =  size(H);
+    L_xc = L_x;
+    for iter=1:Nit
+        for j=1:w
 
-function[Lxc]=calcul_Lxc(trame, )
+            for ci=1:h
+                ind=find(H(ci,:)==1);
+                product = tan(L_xc(ind)/2);
+                p = prod(product);
+                L_cx(ci,j) = 2*atan(p);
+            end  
 
+            for pi=1:w
+                ind=find(H(:,pi)==1);
+                L_cx_ = L_cx(ind);
+                L_xc(j,pi) = L_x(j) + sum(L_cx_(:));
+            end 
+            
+           
+        end
+    end
 end
--- calcul_degre.m
+++ calcul_degre.m
@@ -1,30 +1,39 @@
-function [lambda,pho] = calcul_degre(H)
-format rat
-[nb_parite, nb_variables]=size(H);
+function [lambda,pho,deg_lambda,deg_pho] = calcul_degre(H)
+% format rat
 
-h_col=sum(H);
-[GC,GR] = groupcounts(h_col');
-lengc=length(GC);
-
-for i=1:lengc
-
-    lambda_t(i)=GC(i)/nb_variables;
-    deg_lambda(i) = GR(i)-1;
-end
+[lambda_t,deg_lambda] = lambda_func(H);
+[pho_t, deg_pho] = pho_func(H);
 
 lambda= @(x) lambda_t .* x.^deg_lambda;
-
-
-h_lig=sum(H');
-[GC,GR] = groupcounts(h_lig');
-lengc=length(GC);
-
-for i=1:lengc
-    pho_t(i)=GC(i)/nb_parite
-    deg_pho(i) = GR(i)-1
-end
-
 pho= @(x) pho_t .* x.^deg_pho;
 
 end
 
+function [lambda_t,deg_lambda] = lambda_func(H)
+
+    [nb_parite, nb_variables]=size(H);
+    
+    h_col=sum(H);
+    [GC,GR] = groupcounts(h_col');
+    lengc=length(GC);
+    
+    for i=1:lengc
+    
+        lambda_t(i)=GC(i)/nb_variables;
+        deg_lambda(i) = GR(i)-1;
+    end
+
+end
+
+function [pho_t, deg_pho] = pho_func(H)
+    [nb_parite, nb_variables]=size(H);
+
+    h_lig=sum(H');
+    [GC,GR] = groupcounts(h_lig');
+    lengc=length(GC);
+    
+    for i=1:lengc
+        pho_t(i)=GC(i)/nb_parite
+        deg_pho(i) = GR(i)-1
+    end
+end
